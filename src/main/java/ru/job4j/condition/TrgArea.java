3a7265b7e41cd83d686fc7d56442d87b7a09ed78
nike z <nikegreen@yandex.ru>
2021-10-05 17:25:24 +0500

    9. Площадь треугольника. [#175474]

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -1,6 +1,7 @@
 package ru.job4j.condition;
 
 public class TrgArea {
+
     public static double area(double a, double b, double c) {
         double p = a + b + c;
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
