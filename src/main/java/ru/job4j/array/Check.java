e2151e751a84c3aaecfb7532969656e875984760
nike z <nikegreen@yandex.ru>
2021-10-10 17:09:35 +0500

    6.3. Массив заполнен true или false [#53857 #214327] замена foreach на fori

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -3,11 +3,11 @@ package ru.job4j.array;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean etalon = data[0];
-        for (boolean element : data) {
-            if (etalon != element) {
-                return false;
+            for (int index = 1; index < data.length; index++) {
+                if (etalon != data[index]) {
+                    return false;
+                }
             }
-        }
         return true;
     }
 }
