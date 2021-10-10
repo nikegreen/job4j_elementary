b42dc29ac78e31cefffa9728e845fa7e19150b34
nike z <nikegreen@yandex.ru>
2021-10-10 12:01:27 +0500

    6.0.3. Массивы и цикл for. [#226665] цикл до lenght

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -3,10 +3,10 @@ package ru.job4j.array;
 public class ArrayLoop {
     public static void main(String[] args) {
         int[] numbers = new int[5];
-        for (int index = 0; index < 5; index++) {
+        for (int index = 0; index < numbers.length; index++) {
             numbers[index] = index * 2 + 3;
         }
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
         }
     }
