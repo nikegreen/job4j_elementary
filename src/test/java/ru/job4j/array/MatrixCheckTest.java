965da5adeb1de4dd285472c9065833c5fa28743c
nike z <nikegreen@yandex.ru>
2021-10-13 01:46:16 +0500

    6.7.2. Моно столбец в матрице. [#214127 #214479] добавил тест без моностроки. в функции MonoVertical заменил цикл foreach на fori.

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -41,6 +41,18 @@ public class MatrixCheckTest {
     }
 
     @Test
+    public void whenHasMonoVerticalFalse() {
+        char[][] input = {
+                {'X', ' ', ' '},
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+        };
+        int column = 2;
+        boolean result = MatrixCheck.monoVertical(input, column);
+        Assert.assertFalse(result);
+    }
+
+    @Test
     public void whenDiagonalFullX() {
         char[][] input = {
                 {'X', ' ', ' '},
